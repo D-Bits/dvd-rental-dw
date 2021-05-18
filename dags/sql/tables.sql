f584f033a8723bc218fdb1b381659d8c30d3a568
D-Bits <lockwood.da88@gmail.com>
2021-05-18 13:25:33 -0700

    More data modeling

--- dags/sql/tables.sql
+++ dags/sql/tables.sql
@@ -3,6 +3,7 @@ CREATE TABLE dw.fact_sales
 (
     id SERIAL PRIMARY KEY,
     film_id INT REFERENCES dw.dim_film (id),
+    customer_id INT REFERENCES dw.dim_customer (id),
     rental_date DATETIME NOT NULL,
     return_date DATETIME NOT NULL,
     payment_date DATETIME NOT NULL,
@@ -13,7 +14,7 @@ CREATE TABLE dw.dim_film
 (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    description TEXT,
+    "description" TEXT,
     release_year INT,
     rental_duration SMALLINT,
     rental_rate DECIMAL NOT NULL,
@@ -24,3 +25,15 @@ CREATE TABLE dw.dim_film
     last_update TIMESTAMP,
     special_features TEXT
 );
+
+CREATE TABLE dw.dim_customer
+(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    activebool BOOLEAN,
+    create_date DATE,
+    last_update TIMESTAMP,
+    active INT
+)
