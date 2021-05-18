1f39fe3c4fb7d0ffd245e699521087b0ca9166c0
D-Bits <lockwood.da88@gmail.com>
2021-05-18 13:08:43 -0700

    Started work on OLAP db modeling.

---
+++ dags/sql/tables.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE dw.fact_sales
+(
+    id SERIAL PRIMARY KEY,
+    film_id INT REFERENCES dw.dim_film (id),
+    rental_date DATETIME NOT NULL,
+    return_date DATETIME NOT NULL,
+    payment_date DATETIME NOT NULL,
+    amount DECIMAL NOT NULL
+);
+
+CREATE TABLE dw.dim_film
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    release_year INT,
+    rental_duration SMALLINT,
+    rental_rate DECIMAL NOT NULL,
+    "length" SMALLINT,
+    replacement_cost DECIMAL NOT NULL,
+    -- MPAA rating
+    rating VARCHAR(6),
+    last_update TIMESTAMP,
+    special_features TEXT
+);
