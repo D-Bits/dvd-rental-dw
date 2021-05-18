2e64708e948b1836527532cda70e053996265049
D-Bits <lockwood.da88@gmail.com>
2021-05-18 13:26:11 -0700

    Cleaned up restore script.

--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # Restore Northwind db from backup
-echo "---Restoring Northwind OLTP database---"
-pg_restore --verbose --host=localhost --port=5432 dvdrental.tar --clean --format=t --dbname=dvdrental --user=postgres
+echo "---Restoring DVD Rental OLTP database---"
+pg_restore --verbose --host=localhost --port=5432 dvdrental.tar --clean --format=t --dbname=dvdrentals --user=postgres
